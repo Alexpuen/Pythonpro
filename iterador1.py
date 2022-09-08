0ea94e6944ca0c2e02612e24dea27186ccab7d75
Kaito <alexanderpuentes2004@gmail.com>
2022-09-07 19:21:28 -0500

    Ejercicios iteradores

---
+++ iterador1.py
@@ -0,0 +1,13 @@
+my_list = [1,2,3,4,5]
+my_iter = iter(my_list)
+
+while True:
+    try:
+        element = next(my_iter)
+        print(element)
+    except StopIteration:
+        break
+
+
+for element in my_list:
+    print(element)
